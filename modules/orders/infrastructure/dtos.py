4dbd891195c11c1c41b250e41a47d0d56c8aeb2f
ElReyZero <juan.romero1201@gmail.com>
2023-03-02 15:46:10 +0000

    Fix: Change field to text

--- modules/orders/infrastructure/dtos.py
+++ modules/orders/infrastructure/dtos.py
@@ -1,14 +1,13 @@
-from sqlalchemy import String, Column, Float, Integer
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, Column, Float, Integer, Text
 from config.db import Base
 
 class OrderDTO(Base):
     __tablename__ = "orders"
 
-    order_id = Column(String, primary_key=True, index=True)
-    customer_id = Column(String, index=True)
-    order_date = Column(String, index=True)
-    order_status = Column(String, index=True)
-    order_items = Column(String, index=True)
+    order_id = Column(String(36), primary_key=True, index=True)
+    customer_id = Column(String(36), index=True)
+    order_date = Column(String(100), index=True)
+    order_status = Column(String(100), index=True)
+    order_items = Column(Text(1000000), index=True)
     order_total = Column(Float, index=True)
     order_version = Column(Integer, index=True)
