e78e855c7bdbcc782fb8912ca02640a048ff1267
ElReyZero <31524106+ElReyZero@users.noreply.github.com>
2023-03-12 17:24:40 -0500

    Delete api/errors directory

--- api/errors/exceptions.py
+++
@@ -1,5 +0,0 @@
-class BaseAPIException(Exception):
-    def __init__(self, message: str, code=400):
-        self.error_code = code
-        self.message = message
-        super().__init__(message)
